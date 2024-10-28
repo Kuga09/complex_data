c2094eb5c8fab96a6303c441f2a788d3bc1c06ed
Kuga09 <glal64@tutamail.com>
2024-10-28 21:30:42 +0300

    Лаба

--- 4.py
+++ 4.py
@@ -1,6 +1,4 @@
-input_data = open('input.txt','r') 
-data = input_data.read() 
-N=int(data)
+N=int(input())
 list=[0,1]
 i=0
 i1=1
@@ -16,11 +14,23 @@ for p in range(0,len(list)):
         else:
             a=list[p]**2
             list1.append(a)  
-output_data = open('output1.txt','w')
-output_data.write(str(list))
-output_data.write(" ")
-output_data.write(str(list1))
-output_data.write(" ")
-output_data.write('Длинна списка ='+str(list1))
-input_data.close()
-output_data.close()  
+#max=list1[len(list1)-1]
+max=0
+for d in range(0,len(list1)):
+     if list1[d]>max:
+          max=list1[d]
+min=max
+for d in range(0,len(list1)):
+     if list1[d]<min:
+          min=list1[d]
+if len(list1)%2==0:
+     b=len(list1)/2-1
+     m=(list[b]+list[b+1])/2
+else:
+     b=int((len(list1)-1)/2)
+     m=list[b]
+print(list1)
+print('Длина списка = ', len(list1))
+print('Максимальный элемент = ', max)
+print('Минимальный элемент = ', min)
+print('Медианное значение = ', m)
